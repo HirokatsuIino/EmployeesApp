2511b58d2a4173e108c6bda961b3ea1370769656
hirokatsuiino <letterbox6212>
2021-05-04 20:52:05 +0900

    [ADD]社員詳細情報 migrationファイル

---
+++ EmployeesApp/database/migrations/2021_05_04_111809_create_employee_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('employee_id'); //社員ID
+            $table->string('mail'); //メールアドレス
+            $table->string('tel'); //電話
+            $table->string('gender'); //性別
+            $table->timestamps();
+
+            $table->foreign('employee_id')
+                ->references('id')
+                ->on('employees');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_details');
+    }
+}
