c23a69091a4c5128a0614d004230e708b3b9528b
hirokatsuiino <letterbox6212>
2021-05-05 21:48:56 +0900

    [ADD]登録処理用 ホワイトリスト

--- EmployeesApp/app/Employee.php
+++ EmployeesApp/app/Employee.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
+    protected $fillable = [
+        'employee_id',
+        ];
     //
     public function employee_details()
     {
