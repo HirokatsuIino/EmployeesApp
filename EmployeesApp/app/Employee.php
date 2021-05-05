d433553d36f8dfaa5401cd856d3f4785b9a6bd7a
hirokatsuiino <letterbox6212>
2021-05-05 10:33:13 +0900

    [MODEF]リレーション変更

--- EmployeesApp/app/Employee.php
+++ EmployeesApp/app/Employee.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+//use App\EmployeeDetail;
 
 class Employee extends Model
 {
     //
+    public function employee_details()
+    {
+        return $this->hasOne('App\EmployeeDetail', 'employee_id');
+    }
 }
