1daeb1c33d42f6ff37a63e402f94578fc47a305e
hirokatsuiino <letterbox6212>
2021-05-05 21:49:28 +0900

    [ADD]登録処理用

--- EmployeesApp/app/Http/Controllers/admin/ManageEmployeeController.php
+++ EmployeesApp/app/Http/Controllers/admin/ManageEmployeeController.php
@@ -30,11 +30,28 @@ class ManageEmployeeController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        //社員情報テーブルにデータ格納
+        $item = new Employee();
+        $item->fill($request->all());
+        $item->save();
+
+        //保存した社員情報からIDを取得
+        $detail_Id = $item->id;
+
+        //社員詳細情報テーブルにデータ格納
+        $item_detail = new EmployeeDetail();
+        $item_detail->employee_id = $detail_Id;
+        $item_detail->save();
+
+        return response()->json([
+            'message' => 'OK',
+            'message-code' => 200
+        ], 200, [], JSON_UNESCAPED_UNICODE);
+
     }
 
     /**
